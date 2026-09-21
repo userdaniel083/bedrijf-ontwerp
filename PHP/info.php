90dbff4af41f89605504e1a80ca2e6b2b0dbbfde
userdaniel083 <6050351@mborijnland.nl>
2026-09-21 11:26:09 +0200

    info php

---
+++ PHP/info.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+include '../PDO/database.php';
+?>
+
+<!DOCTYPE html>
+<html lang="nls">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
