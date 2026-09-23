42ddf217f8e968e03b951f8805b875f41fa82072
userdaniel083 <6050351@mborijnland.nl>
2026-09-23 14:48:41 +0200

    fetch: admin account

--- weknemer.sql
+++ weknemer.sql
@@ -1,3 +1,19 @@
-INSERT INTO taxi_db;
+USE taxi_db;
+
+INSERT INTO gebruiker (
+	email,
+	gebruikersnaam,
+	wachtwoord,
+	rol
+)
+VALUES (
+	'admin@taxi.nl',
+	'Admin',
+	'$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2uheWG/igi',
+	'Admin'
+)
+ON DUPLICATE KEY UPDATE
+	gebruikersnaam = 'Admin',
+	rol = 'Admin';
 
 
